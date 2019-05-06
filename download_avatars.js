40e0e204e8b9318f6a7132fb7b33032f58d06ad2
JoshuaHarris93 <josh@emptychairproductions.com>
2019-05-06 23:09:52 +0000

    finally got the avatar urls

--- download_avatars.js
+++ download_avatars.js
@@ -9,18 +9,24 @@ function getRepoContributors(repoOwner, repoName, cb) {
     var options = {
       url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
       headers: {
-        'User-Agent': 'request',
-        'Authorization': 'token 067b03ae617742e2ceeb6327fbb9b53e0d3397a5'
+        'User-Agent': 'request'
       }
     };
   
     request(options, function(err, res, body) {
-      cb(err, body);
+      cb(err, JSON.parse(body));
+
     });
   }
 
+
+
   getRepoContributors("jquery", "jquery", function(err, result) {
+    for (var user of result) {
+        console.log(user.avatar_url)
+    }
+    
     console.log("Errors:", err);
-    console.log("Result:", result);
+
   });
   
