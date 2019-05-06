68c2c2d79807536fab8712f5d8f4dbc6246d147b
JoshuaHarris93 <josh@emptychairproductions.com>
2019-05-06 22:48:33 +0000

    You don't know my secrets..

--- download_avatars.js
+++ download_avatars.js
@@ -1,10 +1,22 @@
 var request = require('request');
 
+var secrets = require('./secrets.js'); 
+
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 
 function getRepoContributors(repoOwner, repoName, cb) {
-    // ...
+    var options = {
+      url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+      headers: {
+        'User-Agent': 'request',
+        'Authorization': 'token 067b03ae617742e2ceeb6327fbb9b53e0d3397a5'
+      }
+    };
+  
+    request(options, function(err, res, body) {
+      cb(err, body);
+    });
   }
 
   getRepoContributors("jquery", "jquery", function(err, result) {
